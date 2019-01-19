12411a35b8c0d9a26f12a1bfb5146f2415de28f2
Chris Prosser <chris@nart.org>
2019-01-19 11:13:28 -0800

    try and make my autodownloader better, fail.

--- tools/fetch_latest_radioid_database.py
+++ tools/fetch_latest_radioid_database.py
@@ -9,10 +9,30 @@ import datetime
 
 script_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
 
+# I don't really understand why there are two databases that are slightly different. 
+# Ham digital is missing columns, so not as good.
+
 # encoding isn't UTF-8, I'm guessing 8859-1 (maybe CP 1252, but hopefully not as this is a web app)
-all_contacts = pd.read_csv("https://www.radioid.net/static/users_quoted.csv", encoding="ISO-8859-1")
+radioid_contacts = pd.read_csv("https://www.radioid.net/static/users_quoted.csv", encoding="ISO-8859-1")
+
+print("Radio ID raw length", len(radioid_contacts))
+# header is repeated occasionally
+radioid_contacts = radioid_contacts[radioid_contacts['Radio ID']!='Radio ID']
+radioid_contacts['Radio ID'] = pd.to_numeric(radioid_contacts['Radio ID'])
+
+print("Radio ID post header scrub", len(radioid_contacts))
+
+ham_digital_contacts = pd.read_csv("https://ham-digital.org/status/users_quoted.csv", 
+                                    encoding="ISO-8859-1",
+                                    names=["Radio ID", "Callsign", "Name", "City", "State", "Country", "Remarks"])
+print("Ham digital length:", len(ham_digital_contacts))
+
 with open("DigitalContactList_UpdateTime.txt", mode="w") as updated_at:
     updated_at.write("%sZ" % datetime.datetime.utcnow())
+
+print("Only using radioid")
+all_contacts = radioid_contacts
+#all_contacts = radioid_contacts.append(ham_digital_contacts)
 
 #all_contacts = pd.read_csv("C:/Users/chris/Downloads/users_quoted.csv", encoding="ISO-8859-1")
 
@@ -42,7 +62,8 @@ w = all_contacts[all_contacts['Callsign']=='VE3VGN']
 # Perry Marvin Rub
 # Change the order to match export so diffs will work
 correct_columns = ["No.","Radio ID","Callsign","Name","City","State","Country","Remarks","Call Type","Call Alert"]
-all_contacts = all_contacts.reindex(columns=correct_columns)
+all_contacts = all_contacts.sort_values(by="Radio ID") \
+                            .reindex(columns=correct_columns)
 
 
 csv_path = os.path.join(script_dir, "../csv_files/DigitalContactList.CSV")
@@ -58,6 +79,9 @@ import unicodedata
 def filt(word):
     return unicodedata.normalize('NFKD', word).encode('ascii', errors='ignore').decode('ascii')
 
-all_contacts.to_csv(csv_path, index=False, quoting = csv.QUOTE_ALL, encoding="ISO-8859-1")
+# switch back to UTF-8
+# The CPS doens't deal with ISO-8859-1 any better and it simplifies my workflow
+# because Meld defaults to UTF-8
+all_contacts.to_csv(csv_path, index=False, quoting = csv.QUOTE_ALL, encoding="utf-8")
 # save off the generated copy to make comparisons with CPS roundtrip easier
-all_contacts.to_csv(csv_path_gen, index=False, quoting = csv.QUOTE_ALL, encoding="ISO-8859-1")
+all_contacts.to_csv(csv_path_gen, index=False, quoting = csv.QUOTE_ALL, encoding="utf-8")
